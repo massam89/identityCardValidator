0201a2b8d436d196983f1bab3614cd2c2fcc8ba6
mas <mas.sam1368@gmail.com>
2021-03-31 22:26:14 +0430

    add more

--- todo-list-vanilla-javascript/index.js
+++ todo-list-vanilla-javascript/index.js
@@ -3,16 +3,17 @@ const exphbs = require('express-handlebars');
 const { v4: uuidv4 } = require('uuid');
 const mysql = require('mysql')
 const app = express();
+require('dotenv').config()
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'masoud',
-  password: 'masoudvash',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
   database: 'todo'
 });
 
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.json());
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
@@ -23,29 +24,51 @@ app.get('/', (req, res) => {
 
   connection.query(queryText, function (error, results, fields) {
     if (error) throw error;
+
+    for (i = 0; i < results.length; i++) {
+      if (results[i].status == 1) {
+        results[i].status = 'checked';
+      } else {
+        results[i].status = '';
+      }
+    }
+
     res.render('home', { item: results });
 
   });
 });
 
 app.post('/addTodo', (req, res) => {
-  const id = uuidv4();
-  const queryText = `INSERT INTO todolist(id, todoText) VALUES(?, ?)`
+  if (req.body.todoText !== '') {
+    const id = uuidv4();
+    const queryText = `INSERT INTO todolist(id, todoText) VALUES(?, ?)`
 
-  connection.query(queryText, [id, req.body.todoText], function (error, results, fields) {
-    if (error) throw error;
-    console.log(results);
-    res.redirect('/')
-  });
+    connection.query(queryText, [id, req.body.todoText], function (error, results, fields) {
+      if (error) throw error;
+      console.log(results);
+      res.redirect('/');
+    });
+  } else {
+    res.redirect('/');
+  }
 })
 
 app.delete('/deleteTodo/:id', (req, res) => {
   const queryText = "DELETE FROM todolist WHERE id = '" + req.params.id + "'";
-  console.log(queryText)
+
   connection.query(queryText, function (error, results, fields) {
     if (error) throw error;
     console.log(results);
-    res.send({ "salam": "good" })
+    res.send({ status: 'done' })
+  });
+})
+
+app.put('/changeStatus/:id', (req, res) => {
+  const queryText = `UPDATE todolist SET status = ${req.body.status} WHERE id = '${req.params.id}'`
+  connection.query(queryText, function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+    res.send({ status: 'done' })
   });
 })
 
